a3887fec02663ee3dd4ba74ca2c84820af3f8649
Eric Schnetzer <eschnetzer@nutraclick.com>
2019-01-02 21:50:53 -0500

    Our Love Story Contd

--- pages/our-love-story.vue
+++ pages/our-love-story.vue
@@ -7,22 +7,68 @@
       <h3>The Beginning</h3>
       <p>They first met in the fall of 2011 at a little bar in downtown Boston called An Tain, the most romantic place on Earth. Eric joined the Wet Willies and they started playing Inner Tube Water Polo together. It’s a vicious sport and Jen is a little bit competitive but luckily, they were on the same team. They had lots of fun together, both in and out of the pool, but Eric found himself stuck in the friend zone early on. That summer, they went on a harbor cruise with a bunch of friends to celebrate Jen’s birthday. Afterwards, they danced together in a fountain on the Greenway in downtown Boston and got soaking wet. It was a really fun night!</p>
 
-      <img src="https://s3.amazonaws.com/eschnetzer/images/fountain.jpg" alt="Dancing in the fountain" style="height: 300px">
+      <img src="https://s3.amazonaws.com/eschnetzer/images/photobomb.jpg" alt="Photobomb" style="height: 280px">
 
-      &nbsp;&nbsp;
+      &nbsp;
 
-      <img src="https://s3.amazonaws.com/eschnetzer/images/photobomb.jpg" alt="Photobomb" style="height: 300px">
+      <img src="https://s3.amazonaws.com/eschnetzer/images/first-cheek-kiss.jpg" alt="The first kiss on the cheek?!" style="height: 280px">
+
+      &nbsp;
+
+      <img src="https://s3.amazonaws.com/eschnetzer/images/fountain.jpg" alt="Dancing in the fountain" style="height: 280px">
 
       <h3>The Courtship</h3>
       <p>They continued to hang out on a regular basis. Eric thought they were going on dates and Jen thought they were just hanging out as friends. After some time had gone by, and some famous Hong Kong scorpion bowls were had, the friendship turned romantic.</p>
 
+      <img src="https://lh3.googleusercontent.com/ybaapd3JeRMwJfDjHcYxLz2QA2ZmMqUBgMQIOJTgBDbRjkttEk5CWIxW5FElxDXTNIlNn76CuKyyBO2EaJzyFC8Du31RpkGNrsX_i3adYFqQDQQNpafrQWJK2FR9QDOMdQNC8VtqKwwp2BRp67JUmBHDKnAzcp60cezYTu_4hksBdFtKz9r2KScYa4whEjYWAaHtm6IiaQFEEWO3vuw7yDVKlanpzIZYtNd4m6u22LrM51aI9mynb_2L6Gayb3m5OhTQ6JNirHAWYeac7PnI-Wl0WTm3AfLW6dsVOdPBYhtPokxoATjpn_6kdr0F0gXInHIyj1Bc3h1W2QmkgOleElV_SFH2xQiYWiJPLvWswMeNMXzZ1ZnWkMP1FiYoUosvn2C21J6wMwjHRMl24-MYhim9iWS0HGuD8TvShWlu23bGJArasPGcuvN04yLurzTFaZdzpOcP60kMmDxWHnvgfR6f0xBtnb5YvXD-mk_B4O9JDj-9DSOX_wYXwJt9gW8TXHcKLVehttFyTEKPV1fVxxHxk4kTGoG7WxSCho5Z_4HjMUK8-D-TAOlNCEYsGrPeem62skhHUbFQ5G8VP5RjZif_mUn94ffibKrNZlJvMCR2ZIMeHzq6lo39uq88KRAjlgYE7ULIWuiu3VLDoemksRCm7axH3lI9HGmDMXIrnQkpVJGL1N8YZImCBwmawDBd7BVr92CbxIv5CxGrbKg=w720-h960-no" alt="Relaxing on the slopes" style="height: 280px">
+
+      &nbsp;
+
+      <img src="https://lh3.googleusercontent.com/tCYtg2j5zixbdeU_UL47iDFsT7cn6T3KuYO_BlrxpvZr9ixX2kWxsHQprfbuay689zD7B3vsszXd4vWW1fQ3rXqSw73hVu27xn4gOrHXsHh-S40vNaBbspf7-BqmvbF73UK8ieN659_ipKktdGDFgi2zZTtHLe510UmHuEl71uWYlceHohmERH2TcB7aBJ-1TbCkEiGvluiA8qiZdN3LAkOfDa_Qq-Smi8CXuVhjMCg-9DUcbBmrwTowneHdagme0Jm-leWVvgA5FiSmPkxMU1MIZCC5ulN7GpBqU5ZVvpBST0oSWT2WOb0MebcLwY5f1_CXiUayO_tY99VDl0X4gAomh_1RCn6HZxjXOFh2dHuHE-nzR7vbD6C2bNtrDLGof0RnKBzSskzHpTiGhbbE0coXwymrDDIZ76yZZZV85m1YmgvdHy6dZ3zE-T7DueurnDg0lC0qXjrMb5tpY8W1nbubEdMoFAKA_a8h6QhMp40XOCUE2CYctWb7IvvpALMhhX9cZJSZnhnpQeZqthIDJJ4w0kiUu4qOZbg8D4twrISaHa0adC-bWhFHPyVDGZDsYNwPoxkcFgg_B2eavk7r-B3dEhRfCadILdTljm14uXi3WZThcwxhEHGj0OldiqegDjjyR_7dVZln0jr-4MFabbwtGuWvA7zxkeCIxY6nIgn4b-stSJOm6lrWSDzI_YazyOg9-HUQOyTtVfnJUDQ=w769-h577-no" alt="Our first Halloween" style="height: 280px">
+
+      &nbsp;
+
+      <img src="https://lh3.googleusercontent.com/Jpmx4SMMM0QO7pHFBPu5iD77hAElonHLpVER_OaGPlKTMI8F9Zli1uZQiTu7Z51l2ocjCuaS-NjsxqNTAHfajYYpm1J5tLJOEJoQnbZX9_36qq9QpaU-CBQoWBLrfOFLHhqWDS_SjfS7sn54wfiWsQcWEkvUnYxSWr0ixKQdbp71tt435faIO1B3TE_Gir_TM2OWSllfs6ByKzWUKNYlnJTSrdg89Tt1-6g38IaBXkQLZyEZLHl5CG1rgFu8b_jXSFvTkqDmAFgoasOwwQfTQd2NxyWOparzJwqwgeaAybbAcgrd8HvbseMZO9qawpzIC-UlEgLkDUAnmh0gwX_p82QojvuX9qzYfDCIqw3OHor5vnGZfyXtdEEC4mgI1tIg18HGbI3vJ_vsSoL_oxsDdoqq9WQqgw-4yk6x3MM0G8GbXFBI0zvlAgHO74ahoWQQLYSlpJ3Bnl4DkHRQUVcuqU2v8WJUPpz2ks1SqGVwiRilKQ4meLRH-KICx78keUIapuote2lrSHn3VDsZz9ch0J0b8aScKqkTFjlWTSe0rUJJBom8_OFzzDY-aNJkwfq0QCPjupBJ248N08PxmkF05e5uVu4k4kq3bLKNKN3VKNmIX5AquRFdOWHu4ZUVJmJ5oIbS7stLkxwFGToguXSM0tEsj54OPEElM1vOOnxjX6QOG1jSqAZIQLBqNSw_x-pAPZi1GZZz1eqoum2e5ls=w613-h460-no" alt="Woudln't date a fan from any other team..." style="height: 280px">
+
       <h3>The Adventures</h3>
       <p>Throughout the years, they have gone on countless adventures together. Some of their favorite past times include exploring new breweries, racing down ski mountains, sitting around a campfire, tailgating at country concerts, taking selfies by scenic waterfalls, going for boat rides on Lake Winnipesaukee, popping bottles of champagne, dancing to cover bands and travelling to new places.</p>
+
+      <img src="https://lh3.googleusercontent.com/RqF1kFm4q4vwbbrs4p9E0zr-k1U34On3u5CbwLgrNyS2QUoP12liXpKjn4g_KI5RpLOcLsyJ9TFWEPsTIxtlbyFklu_2EJb4gJNHNhv9R2uK_a3pT8g26ALjs8mdz56ZZdcn2gXq5xP0pRgOlGolGDn9suzDsEXi1F8xo447ZdCjSDjtd12cDukYz5PyqCl9YpJ02vflxlh3EINhcSgXbfKgurcfePNTefcuD1rpqupiOP6TNytGI3vTQQXRNmUhy8GJ_YAuSIML_smCQUhE3UEFbf2aDqw-Eh129ziU_JfRkhpV2Qvl3J7tx0dsB8SpO4dDqk0g6GLkjj3vUVCgM4mesPXBcQr7fL9b-pNDom-ixjxV7QDie4fVMnB2g3oeXQGO_hpg2MiseO4Pi8EtJl6iAfhU1cDVuoToDyk9E2O3csPgwOK5Dkw8jFLF7gsgan1ok5FXRO-xpg2oy-d9kdACu193GL1mazRSt1kdjP4P3p8jeNixzhTtNkWWzTC_MHuNgn1NIo59R0kd6utOGYTI-Dy6xbZSCHo5gfiXokuCoVm2Eu59o2FQBeyIVF4xGwUutl8Dr9HMaE2W49kL3ZdENmopzw9w1OJAGetYJdS0F5N7OQegVV5Oy0GH4e7COwX6kT_BogbHMM2tffAYX9xhMsMbNekVa5X_dYgNEfneLvlEsl14WmkFQK5D9wwnlc-f9A9fVFesngkCnI8=w769-h577-no" alt="Guatemala" style="height: 240px">
+
+      &nbsp;
+
+      <img src="https://lh3.googleusercontent.com/YGAidf4ZmR0vfhWhr2Te6177GAXuYyQFQV2jvQiocajHau28qVkPWTTCC5vzDfXw4rVkFbed_DIyHaXTFz7sJH9k2yvOpPM8WhbpGwd5yHKdfHQBkpNTeRmdKx1J5DklPFQzdkAibOFBOpi650dtsrCDQdvCbXj7bezgIu_xztabWZ41JjF0QlWdxgLG4Ko05r-O1CYj1oGyDUUinyD2ViCCMFa1hnsHfaWMHRumkO8oTykPPAdeKOBx7n76-RFOFI5xL1nLIs8N-vUuYLOzEHz7OE5UznJb21ZDkggm460LEUuBSHVJJaqcLsn_Gwz4hp1hwU-CfWbtpy37OVEXhbkmMsWaWzTw6JGTaChi91vYDGNMtoqUpO9O7zYVtIo-q-RN_uB0n0d6pg9KQeSNiXMaK3B94IQss6SjinrU5PCQ82DbU02A9S134NvToGDuVR5UzXI1VTJVC1mbjGW_0aLUlP47x8wn9xxrWIbGcPGx1InbYVowxTCW_oRTRjxSQh93f0lT7oFZNIknglqK0k2w3axPJDWqUQSZLN0OIK6F2NFicbvogmGNE50wBKNeO3L-p4ptg1zJA2ud81x_CIrTdYsJPXUMlkr9-5jeF858MQDfOyAiaoe2IMQWJRDdSDUMz38jNXh2JZNEaCqBQ4bvHU4GDLUPVZoJW1JMuIWdAkwXL8DxLXpPlVL7nSbhHJwXyxANRio_KrY9FNk=w615-h462-no" alt="" style="height: 240px">
+
+      &nbsp;
+
+      <img src="https://lh3.googleusercontent.com/EmWXRSThVvGqSAE2WPGexxwvLdM-CoGUVt7iR9d65-l87cAFH7p_NEcLSmOSmXpEXmGggmPNz8wBx-uxXbq3YnpemYff9bm_e64ieYI97WQrCAFyqQ7gQ5hONhY42odoMugGPkvRDApm1s0ukFQUcToDgYyetw8AQZKvGMgZ1lYhed4EpGnMm9fN0CQNg-Fue6C4ruevhAAW2AmmGHkN8DH9WAMOpgXf_XypIyNGn756jqreBaBaRQ3L3st3rMd85KCytT-PVRFSHR7jUXtM6z5gC4K0gxdgiVlKPRhg8olsyrrHuQZBsptQRz0_BU9ltBpraWv2ciIdIdGiIGXOhv1MCuPV0safC39ZScdzxsrxbJEKx7gofNhryj5DgTK-M-q9bBGBkc2qOjOEH6jPNpHv0KYUR1fyI8gWmnPelcgc3ZiobGAk9uIZo-Bd89l4lLHo9Yad-8Q1RrNNr9fiHpD8Fb4qTQI68DbzuZlMKstZx7ENlaKfERMFXKQDSv3Q__-F-Baq1eCsQHpp-SjSrSijFNyHNOwaOEMfRjFfhsI3v4k0Nnizsm2n2rX0Hin54FOVbZLP_U4ZYyoxWFP1DJmb1RbSyZ1DSVXVwygIcNoGVSqTiUjsShj-25sPdBowEYi_S_o9XUAH-FZtNVb5Ld85tFjB6EqHHBSA_GJI_CDI1xaseyBsun6fQjvXATwY9uGZO9mf2xaDp3QAD9E=w769-h577-no" alt="Snowmobiling" style="height: 240px">
 
       <h3>The Engagement</h3>
       <p>Finally, on May 2nd, they got engaged.  Eric had just started a new job at Circle so he asked Jen to come downtown to celebrate. They stopped at Tiki Rock to grab some yummy polynesian drinks.  Ironically, Jen ordered a drink called Saturn and it said, “you’re gonna want to put a ring on it.” Little did she know that Eric had a diamond ring sitting in his pocket. Afterwards, they took a little stroll and stopped by the same fountain they had danced in years earlier.  It was under construction and surrounded by scaffolding but it had always been a special spot for them.  So, that’s where he dropped to a knee and proposed with Jen’s grandmother’s beautiful ring.  Afterwards, they went to White Bull Tavern and watched the Bruins lose a playoff game but Jen wasn’t worried about the game; she was too busy calling friends and family to tell them the good news. Now, it’s time to pop some champagne and celebrate.</p>
 
-      <img src="https://s3.amazonaws.com/eschnetzer/images/engaged.jpg" alt="Engaged at last!" style="height: 300px">
+      <img src="https://lh3.googleusercontent.com/zU1gsK-gp2gPNOLB-_bBfdcLyutwaT_5HJFXBsjESAc1ygxfX1p-Pkumw3S6yYrcKrbG0MQlviyI0q8tdSjGUT0mGdXgB-fsbsTaQshABmnIMcvvGf4Ir5ptkNEEO6hfjVuJtXo4hskh6aGpFPVinSjkqw69RP_0dHMYoq0mBoceJD9tSmeXVWutx10_MVb64s2EPqfdqYukzZdGcagGrMuJJzGmGU8x-YbAu1na0hy8BSnbgVe05yjgz8uKZcyG16ioGs-QJaIv30FXnKt1XOgn4nqcRdYXAw0t_Rj-swKaFTT6eHcsc4jJsrAbJC9jr7R6tkOCfytv6CAR9sF-A4An8pCCP-I-t_XwtABTWw54OVCok5GgIz82bgAM1mbv_KX5YC10rolsuq8eul3HNG_V7_PXmdiquiXzjSqMMj8aZgvTGS0oTIZo4cQTWLDt-g0HXxu_xEQEfryT1kaLImMdnGhFoxkua--sCqXWvYved_qpxhJuOP1pPLvLr6rVkLi91mlD1T-kQMtFkF_iJFq18OTWQ05h96jV5yyLxUaipNuxr_d1NkBbur6WNoH-32FUBY4AeYV9tDi6GuGEajzoCQo5oiWBJ-zznEMHHpqN6RURYbHXw5YAtYBoX4nBJxROqiXdyIKLhMJfNqcJ8tfY9po8vaUCMxSYuY5_eyIcCzvqCUONZbapaFsgyHLBZjANB22LOYHO6CpnquY=w581-h577-no" alt="Engaged at last!" style="height: 280px">
+
+      &nbsp;
+
+      <img src="https://s3.amazonaws.com/eschnetzer/images/engaged.jpg" alt="Engaged at last!" style="height: 280px">
+
+      &nbsp;
+
+      <img src="https://lh3.googleusercontent.com/A21mI9eELEFs_qlLkVgo9fFlWu3688WtVA1WGbInXsWAK9xlZkS86WpxOOtauOx5fklkaMs_s1g6OwfGAIOpMHdGLcPETDhLXSxpOHsuEHsz8MZYJp20rPqzmyAZpUR8HO5RxyBhb_n5lOU-Ncy0aAy3DZBtqTntZvj9grIphT1jKl6_G2Co0iIcyq0zgKkbJXs4U8h7pRR_r2dgE7yzkntMrB8NxNCox2k0doJgvrzYSx7T8D3ByhRPX3dVhoyER5z5RpyfOjQ7t0LsprMlWXSLKqiUh1tui9gYD-WTOs-um8KLj4u8dW7KD6dliVRZgrO5jtJsxXZZLf7DGtJW9-ileyR2h32WZr2nZAphI2g1coBS964zRmnJZovomYVksPhFbvp1i9MC2AxTSqTJH5c8YYfUiIMvXiclXPEFPXua7R0V9Q-3ldCJiHm1XMHnmhOHRWz7khcBhOHjrB7W6_m-ZD7HRdMUjOjMjK-SLacjcDsDh2rDqiLT97V2IRFR7phWcw59x73Q8GcJhjEZUBXXIHWEDRX7it3tG1ncreD6XIutLeqQU9DmSjQVfcVONGikc6S-jxti1nli9DCSdMPdiMqXGfS7TTJmJI6buxnrrALn25SqMVgZ_VwFuHZI114rOjBIzhNry-DilzyJPwGWntTfLjzyPxLiF9anhgCJfuPNBBNKNNHSG49J0toa3PdCRA0wy2CECz07hfk=w769-h577-no" alt="" style="height: 280px">
+
+      <h3>The Future</h3>
+      <p>We are looking forward to a lifetime of fun adventures together and can’t wait to make more memories with all of our friends and family!</p>
+
+      <img src="https://lh3.googleusercontent.com/t1aLDjD3y8inz7ZtWJqChfbcPv2Udzh7AeKUkBIbHB2GF8BNOxFqNZasvRh5VJ4x4zASLwi-qiiSEcgMUBNHYoknAUVyT_FfMyKcRjBSHz9Hdp2AFJOwWD-TAGwDLQXfpXMOJMTWoa2JyUlhy2-vZWn4XMSl6NFb-q1kW8WO8Vs7ETIRGajxDUd_TvAvqxUFfoLdh0GVa4MlRWlZq5zZx-ERXVJrwK0phED-j7M0dtrcNbqIzQ3Rno7pEz9WngFgTvFyi2tjxukDOdyvbK9VH5hgncIHDM9gDkKRNgYGmxJaj5iq-wnjHfLe9inW1UxxovO26G_UukU1gVQQSiGCRFVkitF8bVGH3kbq1uTG4PihwVntgN3AuHKrOPZ5OWpIBjG0zhYmQw5qUt5s-be_gsV7-aepDbwF_hi41sFHrrqD1NO__2B3u6oQBaCbXceouZNpUGbrEYYrZEyydibs7Ow_qvvvavc5iVTCJs0CtzvyLE1gM9W3UeJDn45ek5AybMbcx2xsmJGirjHidvPMPwFAZ_K25RPsaea5oKRzVmSDV0FnzIqExXekZ-hcrdMfloh696-fWcezCdm3EQelL6MVK9UEmi330QXjy6g46h-EA__L4K70rADaePVx-7Q6NL_I0pceMJ36XD6Bt5N5SEbkVo1hwMnLZFVPRkFcNvxj9geQuBmbReWDNvqMSzcb3p9P6vw4DHb5iuu1Aac=w769-h577-no" alt="" style="height: 280px">
+
+      &nbsp;
+
+      <img src="https://lh3.googleusercontent.com/vkBkZi7v0nXGeTTgHtGH_6F4Q2WTlwUNTkHAMz4_Jirm-M_w93fNa5EUb1JgX2OpZt9Siymsrg6xBl_o7izuTIphdPez3Fu3ToumpHi2p5Hj4YHP_CAZ1hGnm3N1VKEor9DDHLr_mgauzxoUQKcjFsCrG3oglCyPSh457hbnB-FymguVWQzWRVZT7KiKpLiiUSB-hUFubvz7tZH90K3WSIFWwwiN3cj-WVeOIVwJ8-INp5ziCLYSTHo8wRxmWbJi9v9tkMR_t3Emr8OjiINp_dvg1B0NgoeFMj3MvqGs8pYSaWv0nJcaZUJMKxH7KPwYU0WBQe6tXu713D2LoP6RyFVUUPiVYvJamkv7x-XtqjPgdEsavccb8DAHh5yxOAUg9gY17Zjw2paVx-Ph36r5h0kshbtW9xU7BEOLH93VXOeMbrftZxVLp-B4Z23T_8DIXNXvT0NZrkbQjFKPF9SfWXIbuk_N1S4GaqJ3HPykMXlPRupVWchfjdkud6IJUzLC9pTa8wd5ABHUkheX3QYRzeAQ-J4LSsCf1kjb7560FupoTidvNibagNcH65ujP4OK-3Zf8gJ-2-xmlyauz8VChBZWhEMeHNqeDzmuF553S6rw6iITpxgC_Bp0fmzCN6sAU_U_Xgz0HHUjA0bx-AXVE2w5paIhI59SCOZUdh3eeFFWZ1W7svpLKrp7sIjDr92Zxk3g4owhxK_aT8CV0G8=w677-h577-no" alt="" style="height: 280px">
+
+      &nbsp;
+
+      <img src="https://s3.amazonaws.com/eschnetzer/images/oldfolks.jpg" alt="Runnin'" style="height: 280px">
+
     </div>
   </section>
 </template>
