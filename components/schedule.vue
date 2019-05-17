881dbe779cb168b96512e9ef2d1dd9909695ca04
Eric Schnetzer <eschnetzer@nutraclick.com>
2019-05-16 23:11:47 -0400

    Updated Schedule and Lodging

--- components/schedule.vue
+++ components/schedule.vue
@@ -1,24 +1,81 @@
 <template>
   <div class="schedule">
+    <br />
+
+    <h4>Thursday, July 18th, 2019</h4>
+
+    <h3>Kick off the Weekend</h3>
+    <p>
+      <a href="https://goo.gl/maps/k7CbzGMbqDoKDBc29" target="_blank">
+        Winnipesaukee Belle
+        <br />
+        4 N Main St, Wolfeboro, NH 03894
+      </a>
+    </p>
+    <p>Dance along to Dancing Madly Backwards out on Lake Winnipesaukee on a replica turn-of-the-century paddle boat.</p>
+    <p>Tickets $18, pay as you board. Cash bar. 21+.</p>
+    <p>6:30 pm</p>
+
+    <br /><br />
+
+    <h4>Friday, July 19th, 2019</h4>
+
+    <h3>Welcome Party</h3>
+    <p>
+      <a href="https://goo.gl/maps/k7CbzGMbqDoKDBc29" target="_blank">
+        The Cain Residence
+        <br />
+        212 Forest Rd, Wolfeboro, NH 03894
+      </a>
+    </p>
+    <p>If you are in town, please join us for a welcome party with pizza and music. <br /> If we're lucky, we might even catch a sunset at Carry Beach. <br /> Bring a camera!</p>
+    <p>Please make sure to let us know if you'll be joining us!</p>
+    <p>7 pm</p>
+
+    <br /><br />
+
     <h4>Saturday, July 20th, 2019</h4>
 
     <h3>Ceremony</h3>
-    <p>St. Katharine Drexel Parish</p>
-    <p>40 Hidden Springs Rd, Alton, NH 03809</p>
+    <p>
+      <a href="https://goo.gl/maps/uNXXU4zPRV14ima36" target="_blank">
+        St. Katharine Drexel Parish
+        <br />
+        40 Hidden Springs Rd, Alton, NH 03809
+      </a>
+    </p>
     <p>1 pm</p>
 
     <h3>Cocktail Hour</h3>
-    <p>On the lawn at Akwa Marina Yacht Club</p>
-    <p>95 Centenary Ave, Laconia, NH 03246</p>
+    <p>On the lawn at <a href="https://goo.gl/maps/HCTWUACcSDKKUgNP8" target="_blank">Akwa Marina Yacht Club</a> <br /> <a href="https://goo.gl/maps/HCTWUACcSDKKUgNP8" target="_blank">95 Centenary Ave, Laconia, NH 03246</a></p>
     <p>4 pm</p>
 
     <h3>Reception</h3>
-    <p>Under a tent at Akwa Marina</p>
+    <p>Under a tent at <a href="https://goo.gl/maps/HCTWUACcSDKKUgNP8" target="_blank">Akwa Marina</a> <br /> Bring your dancing shoes!</p>
     <p>5 pm - 10 pm</p>
 
     <h3>After Party</h3>
-    <p>Beach Bar at Akwa Marina</p>
+    <p>Beach Bar at <a href="https://goo.gl/maps/HCTWUACcSDKKUgNP8" target="_blank">Akwa Marina</a></p>
     <p>10 pm - 1 am</p>
+
+    <br /><br />
+
+    <h4>Sunday, July 21st, 2019</h4>
+
+    <h3>Goodbye Brunch and Lake Day</h3>
+    <p>
+      <a href="https://goo.gl/maps/k7CbzGMbqDoKDBc29" target="_blank">
+        The Cain Residence
+        <br />
+        212 Forest Rd, Wolfeboro, NH 03894
+      </a>
+    </p>
+    <p>We don't want the fun to end so join us for a farewell brunch by the lake.</p>
+    <p>Bring a swimsuit!</p>
+    <p>Please make sure to let us know if you'll be joining us!</p>
+    <p>11 am</p>
+
+    <br />
   </div>
 </template>
 
--- pages/accommodations.vue
+++ pages/accommodations.vue
@@ -6,6 +6,36 @@
       </h2>
       <p>It’s summertime in the Lakes Region and it’s also Nascar race weekend so some places are considering it a holiday and may have a 2 to 3 night minimum. Airbnb or Homeaway are always good options to find unique places to stay.</p>
 
+      <p>We will provide transportation from the hotels in Wolfeboro to the wedding reception in Laconia and back at the end of the night on Saturday. Please let us know if you're interested in this service.</p>
+
+      <div>
+        <h3 class="alignCenter divider">
+          In Wolfeboro
+        </h3>
+
+        <h4 class="alignCenter">
+          <a href="https://lakewentworthinn.com/" target="_blank">
+            Lake Wentworth Inn
+          </a>
+        </h4>
+        <p>
+          We have a block of rooms available at this cute, newly-renovated motel. The rooms are completely brand new and are located on Lake Wentworth with beach access, a pool, and is pet friendly. Please let us know if you are interested in staying here to make sure you are offered one of the rooms in our block.
+        </p>
+
+        <br />
+
+        <h4 class="alignCenter">
+          <a href="https://www.crescentlakeinn.com/" target="_blank">
+            Crescent Lake Inn &amp; Suites
+          </a>
+        </h4>
+
+        <p>
+          Located in the beautiful Lake Winnipesaukee area in Wolfeboro, NH, Crescent Lake Inn &amp; Suites feature guest rooms situated on expansive lawns overlooking Crescent Lake.
+        </p>
+
+      </div>
+
       <div>
         <h3 class="alignCenter divider">
           In Laconia
@@ -54,33 +84,6 @@
           <a href="https://naswa.com/" target="_blank">
             The NASWA Resort
           </a>
-        </p>
-
-      </div>
-
-      <div>
-        <h3 class="alignCenter divider">
-          In Wolfeboro
-        </h3>
-
-
-        <h4 class="alignCenter">
-          Lake Wentworth Inn
-        </h4>
-        <p>
-          Formerly known as the Allen A Motor Inn, this motel was recently bought by new owners who are doing lots of renovations. The Grand Opening will be Memorial Day weekend and it has 42 rooms available at a reasonable price.  We are looking into getting a block of rooms so please let us know if you are interested in staying here.
-        </p>
-
-        <br />
-
-        <h4 class="alignCenter">
-          <a href="https://www.crescentlakeinn.com/" target="_blank">
-            Crescent Lake Inn &amp; Suites
-          </a>
-        </h4>
-
-        <p>
-          Located in the beautiful Lake Winnipesaukee area in Wolfeboro, NH, Crescent Lake Inn &amp; Suites feature guest rooms situated on expansive lawns overlooking Crescent Lake.
         </p>
 
       </div>
