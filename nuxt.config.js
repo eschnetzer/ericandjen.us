ffeaaeb1685fe436a4c034cff3fdf0f27ee2363d
Eric Schnetzer <eschnetzer@nutraclick.com>
2019-06-06 17:27:51 -0400

    Remove trailing slashes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,59 +1,85 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: 'Jennifer Cain and Eric Schnetzer | July 20, 2019',
+    title: "Jennifer Cain and Eric Schnetzer | July 20, 2019",
     meta: [
-      { charset: 'utf-8' },
+      { charset: "utf-8" },
       {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
       },
-      { hid: 'description', name: 'description', content: 'The wedding of Jennifer Cain and Eric Schnetzer' },
-      { name: 'google-site-verification', content: 'Bu31_Jp5FIz4DwBJnt3GCbzf3sU6jeLWanFtRmwsqsc' }
+      {
+        hid: "description",
+        name: "description",
+        content: "The wedding of Jennifer Cain and Eric Schnetzer"
+      },
+      {
+        name: "google-site-verification",
+        content: "Bu31_Jp5FIz4DwBJnt3GCbzf3sU6jeLWanFtRmwsqsc"
+      }
     ],
     link: [
-      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico?v3' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Dancing+Script' }
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png"
+      },
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico?v3" },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css?family=Dancing+Script"
+      }
     ]
   },
-  css: [
-    '@/assets/css/style.css'
-  ],
+  css: ["@/assets/css/style.css"],
   /*
-  ** Customize the progress bar color
-  */
-  loading: { color: 'rgb(253, 255, 137)' },
+   ** Customize the progress bar color
+   */
+  loading: { color: "rgb(253, 255, 137)" },
   plugins: [
-    { src: '~/plugins/fontawesome.js' },
-    { src: '~/plugins/moment.js' }
+    { src: "~/plugins/fontawesome.js" },
+    { src: "~/plugins/moment.js" }
   ],
   modules: [
-    ['@nuxtjs/google-analytics', {
-      id: 'UA-126963860-1'
-    }]
+    [
+      "@nuxtjs/google-analytics",
+      {
+        id: "UA-126963860-1"
+      }
+    ],
+    // makes a 301 redirection to a non trailing slash URL
+    "nuxt-trailingslash-module"
   ],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
+     ** Run ESLint on save
+     */
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
     }
   }
-}
-
+};
